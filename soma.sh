c42aa485433c5e5396c15189ac2de511176b8e60
Renan Pallin <renanpallin@gmail.com>
2017-12-23 15:02:52 -0200

    Estvam aqui

---
+++ soma.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+for i in `seq 1000`; do
+	if [[ $(($i%2)) ]]; then
+		#statements
+	fi
+
+done
+
---
+++ terminal_calculator.js
@@ -0,0 +1,7 @@
+var x = function(n){
+	var soma = 0;
+	for (i=0; i<=n; i++){
+		soma += i;
+	}
+	console.log(n+"? = " + soma);
+}
